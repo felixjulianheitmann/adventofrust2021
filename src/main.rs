f2fb8fffe4ee678748bf525fd34815af73dee11b
Felix <felixjulianheitmann@gmail.com>
2022-07-10 22:54:20 +0200

    Solution to puzzle part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+pub mod util;
+
 fn main() {
-    println!("Hello, world!");
+    let lines: Vec<i32> = util::split_lines(util::read_input());
+
+    let mut n_inc = 0;
+    let mut prev = lines[0];
+
+    for depth in &lines[1..] {
+        if prev < *depth {
+            n_inc += 1;
+        }
+        prev = *depth;
+    }
+
+    util::write_output(n_inc);
+
 }
