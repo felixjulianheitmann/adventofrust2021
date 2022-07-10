de6864a3d2463349b360bffa476bbfd206b05a85
Felix <felixjulianheitmann@gmail.com>
2022-07-10 22:53:50 +0200

    implemented additional functionalite in utils - split lines to arbitrary type - split at token to arbitrary type - write output util

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,23 @@
-
 pub fn read_input() -> String {
     std::fs::read_to_string("input.txt").expect("Something went wrong reading the input...")
 }
 
-pub fn split_lines(text: &str) -> Vec<&str> {
-    text.split('\n').collect()
+pub fn split_lines<T>(text: String) -> Vec<T>
+where T: std::str::FromStr,
+<T as std::str::FromStr>::Err: std::fmt::Debug
+{
+    let lines = text.split('\n');
+    lines.map(|l| l.parse().unwrap()).collect()
+}
+    
+pub fn split_lines_str(text: String) -> Vec<String> {
+    text.split('\n').map(|l| String::from(l)).collect()
 }
 
-pub fn split_at(token: char, text: &str) -> Vec<&str> {
+pub fn split_at<T>(token: char, text: &str) -> Vec<&str> {
     text.split(token).collect()
+}
+
+pub fn write_output<T>(out: T) where T: std::fmt::Display {
+    std::fs::write("output.txt", format!("The result is: {}", out.to_string())).expect("Writing to output.txt failed...")
 }
