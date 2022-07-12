f385cfe7220fef7531824e5bc268124f0f78e6c3
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-12 19:35:47 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+pub mod util;
+
 fn main() {
-    println!("Hello, world!");
+    let lines = util::split_lines_str(util::read_input());
+    let draws = util::split_at::<&i32>(',', &lines[0]);
+
+    for draw in draws {
+        for mut board in lines[1..].chunks(6) {
+            board = &board[1..];
+            // use vector of vectors for representation
+        }
+    }
+
 }
