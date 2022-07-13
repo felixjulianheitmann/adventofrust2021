1590749559b6e71452bcdc94a62f29b84a53e22e
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-13 22:36:32 +0200

    Implemented puzzle solution

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn sort(lhs: usize, rhs: usize) -> (usize, usize) {
 }
 
 fn main() {
+    let grid_size = 1000;
+
     let vents: Vec<Vec<_>> = 
         util::split_lines_str(util::read_input()).iter()
             .map(|l| util::split_at::<String>(" -> ", l.to_string())
@@ -17,22 +19,38 @@ fn main() {
 
     // initialze ocean floor
     let mut geo: Vec<Vec<i32>> = Vec::new();
-    geo.resize(1000, Vec::new());
-    geo.iter_mut().for_each(|r| r.resize(1000, 0));
+    geo.resize(grid_size, Vec::new());
+    geo.iter_mut().for_each(|r| r.resize(grid_size, 0));
 
+    // count vents
     for vent in vents {
-        if vent[0][0] != vent[1][0] && vent[0][1] != vent[1][1] {
-            continue;
-        }
-        let row = sort(vent[0][0], vent[1][0]);
-        let col = sort(vent[0][1], vent[1][1]);
-        for r in row.0..=row.1 {
-            for c in col.0..=col.1 {
-                geo[r][c] += 1;
+        let (row, col) = ((vent[0][0], vent[1][0]), (vent[0][1], vent[1][1]));
+
+        if row.0 != row.1 && col.0 != col.1 {
+            // diagonal line
+            let row_r = match row.0 > row.1 {
+                true => (row.1..=row.0).rev().collect::<Vec<usize>>(),
+                false => (row.0..=row.1).collect::<Vec<usize>>(),
+            };
+            let col_r = match col.0 > col.1 {
+                true => (col.1..=col.0).rev().collect::<Vec<usize>>(),
+                false => (col.0..=col.1).collect::<Vec<usize>>(),
+            };
+
+            for (r, c) in row_r.iter().zip(col_r.iter()) {
+                geo[*r][*c] += 1;
+            }
+        } else {
+            // horizontal/vertical line
+            let row = sort(row.0, row.1);
+            let col = sort(col.0, col.1);
+            for r in row.0..=row.1 {
+                for c in col.0..=col.1 {
+                    geo[r][c] += 1;
+                }
             }
         }
     }
 
     util::write_output(geo.iter().flatten().map(|x| *x).filter(|x| *x > 1).count());
-
 }
