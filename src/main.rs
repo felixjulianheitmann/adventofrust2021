6c3305205357947b7f1b7202446c1e6e619d9472
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-14 12:29:06 +0200

    Implemented puzzle solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+pub mod util;
+
 fn main() {
-    println!("Hello, world!");
+    let crabs: Vec<i32> = util::split_at(",", util::read_input());
+    let max_pos = crabs.iter().max().unwrap();
+    let mut costs: Vec<i32> = Vec::new();
+    costs.resize((*max_pos + 1) as usize, 0);
+
+    for pos in 0..=*crabs.iter().max().unwrap() {
+        costs[pos as usize] = crabs
+            .iter()
+            .fold(0, |acc, crab_pos| acc + (pos - crab_pos).abs());
+    }
+
+    util::write_output(costs.iter().min().unwrap());
 }
