3beef041e52b213a21e26f425990d9e0155e5192
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-14 16:36:56 +0200

    Implemented puzzle solution

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,9 @@ fn main() {
         })
         .collect::<Vec<Vec<_>>>();
 
-    let mut risk = 0;
+    let mut basins: Vec<i32> = Vec::new();
+
+    // find lowpoints
     for r in 0..heights.len() {
         for c in 0..heights[0].len() {
             let top = match heights.get(((r as i32) - 1) as usize) {
@@ -44,10 +46,103 @@ fn main() {
             };
             let this = heights[r][c];
             if this < top && this < bottom && this < left && this < right {
-                risk += this + 1;
+                // Calculate basin size
+                let mut checked_map: Vec<Vec<bool>> = Vec::new();
+                checked_map.resize(heights.len(), Vec::new());
+                checked_map
+                    .iter_mut()
+                    .for_each(|v| v.resize(heights[0].len(), false));
+                checked_map[r][c] = true;
+                let mut size = 1;
+
+                basins.push(calc_basin_size(
+                    &r,
+                    &c,
+                    &heights,
+                    &mut checked_map,
+                    &mut size,
+                ));
             }
         }
     }
 
-    util::write_output(risk);
+    // Calculate result
+    let first = *basins.iter().max().unwrap();
+    basins.remove(basins.iter().position(|x| *x == first).unwrap());
+    let second = *basins.iter().max().unwrap();
+    basins.remove(basins.iter().position(|x| *x == second).unwrap());
+    let third = *basins.iter().max().unwrap();
+    util::write_output(first * second * third);
+}
+
+fn calc_basin_size(
+    x: &usize,
+    y: &usize,
+    heights: &Vec<Vec<i32>>,
+    checked_map: &mut Vec<Vec<bool>>,
+    basin_size: &mut i32,
+) -> i32 {
+    let field_check = |height: i32, checked_yet: &mut bool, basin_size: &mut i32| -> bool {
+        let mut still_basin = false;
+        if height < 9 && !*checked_yet {
+            *basin_size += 1;
+            still_basin = true;
+        }
+        *checked_yet = true;
+        still_basin
+    };
+
+    // Check neighbors
+    match heights.get(((*x as i32) - 1) as usize) {
+        Some(col) => {
+            if field_check(
+                *col.get(*y).unwrap(),
+                checked_map.get_mut(x - 1).unwrap().get_mut(*y).unwrap(),
+                basin_size,
+            ) {
+                calc_basin_size(&(x - 1), y, heights, checked_map, basin_size);
+            }
+        }
+        None => (),
+    };
+
+    match heights.get(x + 1) {
+        Some(col) => {
+            if field_check(
+                *col.get(*y).unwrap(),
+                checked_map.get_mut(x + 1).unwrap().get_mut(*y).unwrap(),
+                basin_size,
+            ) {
+                calc_basin_size(&(x + 1), y, heights, checked_map, basin_size);
+            }
+        }
+        None => (),
+    };
+
+    match heights.get(*x).unwrap().get(((*y as i32) - 1) as usize) {
+        Some(val) => {
+            if field_check(
+                *val,
+                checked_map.get_mut(*x).unwrap().get_mut(y - 1).unwrap(),
+                basin_size,
+            ) {
+                calc_basin_size(x, &(y - 1), heights, checked_map, basin_size);
+            }
+        }
+        None => (),
+    };
+    match heights.get(*x).unwrap().get(y + 1) {
+        Some(val) => {
+            if field_check(
+                *val,
+                checked_map.get_mut(*x).unwrap().get_mut(y + 1).unwrap(),
+                basin_size,
+            ) {
+                calc_basin_size(x, &(y + 1), heights, checked_map, basin_size);
+            }
+        }
+        None => (),
+    };
+
+    *basin_size
 }
