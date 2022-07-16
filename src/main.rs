fbd160de39d700ec17db182a6c672395f1b274b6
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-16 11:15:12 +0200

    Implemented puzzle solution - Only add score for syntax errors, not for uncompleted lines

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,63 @@
+use std::iter::Peekable;
+
 pub mod util;
 
 fn main() {
     let lines = util::split_lines_str(util::read_input());
     let mut score = 0;
     for line in lines {
-        let (mut score_l, mut it_rest) = check_block(&line.chars());
+        let mut it = line.chars().peekable();
+        let (score_l, _) = check_block(it.next().unwrap(), it);
         score += score_l;
-        while let Some(_) = it_rest.peekable().peek() {
-            (score_l, it_rest) = check_block(it_rest);
-            score += score_l;
-        }
     }
 
     util::write_output(score);
 }
 
-fn check_block<'a>(it: &'a std::str::Chars<'a>) -> (i32, &'a std::str::Chars<'a>) {
-
-    let ot = it.next().unwrap();
-
+fn check_block<'a>(
+    ot: char,
+    mut it: Peekable<std::str::Chars<'a>>,
+) -> (i32, Peekable<std::str::Chars<'a>>) {
+    let mut score = 0;
     loop {
-
-        let nt = match it.next() {
-            Some(t) => t,
-            None => return (symbol2score(ot), it),
-        };
-        match token {
-            '(' => match nt {
-                ')' => (0, &line[1..]),
-                ']' | '}' | '>' => (symbol2score(nt), &line[1..]),
-                _ => {
-                    let (score, rest) = check_block(nt, &line[1..]);
-                    match rest.chars().next() {
-                        Some(t) => 
-                    }
-    
+        if let Some(_) = it.peek() {
+            let nt = it.next().unwrap();
+            // Is block opening token?
+            if nt == '(' || nt == '[' || nt == '{' || nt == '<' {
+                let score_tmp;
+                (score_tmp, it) = check_block(nt, it);
+                score += score_tmp;
+            }
+            // Is block closing token?
+            else {
+                match ot {
+                    '(' => match nt {
+                        ')' => return (score, it),
+                        ']' | '}' | '>' => return (score + symbol2score(nt), it),
+                        _ => return (score, it),
+                    },
+                    '[' => match nt {
+                        ']' => return (score, it),
+                        ')' | '}' | '>' => return (score + symbol2score(nt), it),
+                        _ => return (score, it),
+                    },
+                    '{' => match nt {
+                        '}' => return (score, it),
+                        ')' | ']' | '>' => return (score + symbol2score(nt), it),
+                        _ => return (score, it),
+                    },
+                    '<' => match nt {
+                        '>' => return (score, it),
+                        ')' | ']' | '}' => return (score + symbol2score(nt), it),
+                        _ => return (score, it),
+                    },
+                    _ => return (score, it),
                 }
-            },
-            '[' => match nt {
-                ']' => 0,
-                ')' | '}' | '>' => symbol2score(nt),
-                _ => {
-                    let (score, rest) = check_block(nt, &line[1..]);
-    
-                }
-            },
-            '{' => match nt {
-                '}' => 0,
-                ')' | ']' | '>' => symbol2score(nt),
-                _ => {
-                    let (score, rest) = check_block(nt, &line[1..]);
-    
-                }
-            },
-            '<' => match nt {
-                '>' => 0,
-                ')' | ']' | '}' => symbol2score(nt),
-                _ => {
-                    let (score, rest) = check_block(nt, &line[1..]);
-    
-                }
-            },
-            _ => 0,
+            }
+        // No token left
+        } else {
+            return (score, it);
+            // return (score + symbol2score(ot), it);
         }
     }
 }
