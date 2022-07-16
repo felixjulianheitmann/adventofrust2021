2bf6f5b1599d9c662176293a30d744e05cd587b3
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-16 12:19:06 +0200

    Implemented puzzle solution

--- src/main.rs
+++ src/main.rs
@@ -4,74 +4,79 @@ pub mod util;
 
 fn main() {
     let lines = util::split_lines_str(util::read_input());
-    let mut score = 0;
+    let mut scores: Vec<usize> = Vec::new();
     for line in lines {
         let mut it = line.chars().peekable();
-        let (score_l, _) = check_block(it.next().unwrap(), it);
-        score += score_l;
+        let mut score_acc = 0;
+        while !it.peek().is_none() {
+            if let Some((score_l, it_rest)) = check_block(it.next().unwrap(), it) {
+                it = it_rest;
+                score_acc += score_l;
+            } else {
+                break;
+            }
+        }
+        if score_acc > 0 {
+            scores.push(score_acc);
+        }
     }
 
-    util::write_output(score);
+    scores.sort();
+    util::write_output(scores[(scores.len() - 1) / 2]);
 }
 
 fn check_block<'a>(
     ot: char,
     mut it: Peekable<std::str::Chars<'a>>,
-) -> (i32, Peekable<std::str::Chars<'a>>) {
+) -> Option<(usize, Peekable<std::str::Chars<'a>>)> {
     let mut score = 0;
     loop {
         if let Some(_) = it.peek() {
             let nt = it.next().unwrap();
             // Is block opening token?
             if nt == '(' || nt == '[' || nt == '{' || nt == '<' {
-                let score_tmp;
-                (score_tmp, it) = check_block(nt, it);
-                score += score_tmp;
+                if let Some((score_tmp, it_rest)) = check_block(nt, it) {
+                    score += score_tmp;
+                    it = it_rest;
+                } else {
+                    return None;
+                }
             }
             // Is block closing token?
             else {
                 match ot {
                     '(' => match nt {
-                        ')' => return (score, it),
-                        ']' | '}' | '>' => return (score + symbol2score(nt), it),
-                        _ => return (score, it),
+                        ')' => return Some((0, it)),
+                        _ => return None,
                     },
                     '[' => match nt {
-                        ']' => return (score, it),
-                        ')' | '}' | '>' => return (score + symbol2score(nt), it),
-                        _ => return (score, it),
+                        ']' => return Some((0, it)),
+                        _ => return None,
                     },
                     '{' => match nt {
-                        '}' => return (score, it),
-                        ')' | ']' | '>' => return (score + symbol2score(nt), it),
-                        _ => return (score, it),
+                        '}' => return Some((0, it)),
+                        _ => return None,
                     },
                     '<' => match nt {
-                        '>' => return (score, it),
-                        ')' | ']' | '}' => return (score + symbol2score(nt), it),
-                        _ => return (score, it),
+                        '>' => return Some((0, it)),
+                        _ => return None,
                     },
-                    _ => return (score, it),
+                    _ => return None,
                 }
             }
         // No token left
         } else {
-            return (score, it);
-            // return (score + symbol2score(ot), it);
+            return Some((score * 5 + symbol2score(ot), it));
         }
     }
 }
 
-fn symbol2score(s: char) -> i32 {
+fn symbol2score(s: char) -> usize {
     match s {
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
-        '(' => 3,
-        '[' => 57,
-        '{' => 1197,
-        '<' => 25137,
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
         _ => 0,
     }
 }
