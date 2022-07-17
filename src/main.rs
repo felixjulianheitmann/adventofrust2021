1587d4c0b604dc9889ee69704e2bc8e92cfc5b5f
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-17 09:42:37 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
 pub mod util;
 
 fn main() {
-    let input = util::read_input();
+    let vertices = util::split_lines_str(util::read_input())
+        .iter()
+        .map(|s| util::split_at::<String>("-", *s))
+        .collect::<Vec<Vec<_>>>();
 
-    util::write_output(input);
+    let caves: Vec<Cave> = Vec::new();
+
+    for vertice in vertices {
+        let start = &vertice[0];
+        let end = &vertice[1];
+        if 
+    }
+
+    util::write_output(0);
+}
+
+struct Cave<'a> {
+    name: String,
+    visited: bool,
+    is_large: bool,
+    neighbors: Vec<&'a Cave<'a>>,
+}
+
+impl Vec<Cave> {
+    fn in_list(&self, c: &Cave) -> bool {
+        &self.iter().any(|x| x.name == c.name)
+    }
+
+    fn add_vertice(&self, start: &str, end: &str) {
+        match &self.iter().find(|x| &x.name == start) {
+            Some(c) => *c.neighbors.push(Cave::new(start));
+        }
+    }
 }
