626d4d61cb4f89158c3eef59795323a7e5117715
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-21 23:58:16 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,69 @@
 pub mod util;
 
-use std::{collections::HashSet, io::BufRead};
+use std::collections::HashSet;
 
 fn main() {
+    type DotSet = HashSet<(i32, i32)>;
+
     let input = util::split_at::<String>("\n\n", util::read_input());
-    let mut dots = util::split_lines_str(input[0])
+    let mut dots = util::split_lines_str(input[0].to_string())
         .iter()
         .map(|l| {
-            let l = l.split(',');
+            let mut l = l.split(',');
             (
                 l.next().unwrap().parse().unwrap(),
                 l.next().unwrap().parse().unwrap(),
             )
         })
-        .collect::<HashSet<(i32, i32)>>();
+        .collect::<DotSet>();
 
-    let folds = util::split_lines_str(input[1])
-        .iter()
+    let folds = util::split_lines_str(input[1].to_string())
+        .into_iter()
         .map(|l| {
-            let l = l.strip_prefix("fold along ").unwrap().split('=');
-            (l.next().unwrap(), l.next().unwrap().parse().unwrap())
+            let mut l = l.strip_prefix("fold along ").unwrap().split('=');
+            (
+                l.next().unwrap().to_string(),
+                l.next().unwrap().parse().unwrap(),
+            )
         })
-        .collect::<Vec<(&str, i32)>>();
+        .collect::<Vec<(String, i32)>>();
 
     // Start the folding
     for (axis, fold) in folds {
-        match axis {
-            "y" => {
-                for (x, y) in dots.into_iter().filter(|(x, y)| *x < fold) {
-                    dots.insert((2 * fold - x, y));
-                    dots.remove(&(x, y));
+        let mut new_dots: DotSet = HashSet::new();
+
+        for (x, y) in dots {
+            match axis.as_str() {
+                "y" => {
+                    if x < fold {
+                        new_dots.insert((2 * fold - x, y));
+                        new_dots.remove(&(x, y));
+                    }
                 }
-            }
-            "x" => {
-                for (x, y) in dots.into_iter().filter(|(_, y)| *y > fold) {
-                    dots.insert((x, 2 * fold - y));
-                    dots.remove(&(x, y));
+                "x" => {
+                    if y > fold {
+                        new_dots.insert((x, 2 * fold - y));
+                        new_dots.remove(&(x, y));
+                    }
                 }
-            }
-        };
+                _ => (),
+            };
+        }
+
+        dots = new_dots;
+        break;
     }
 
-    util::write_output(input);
+    for i in 0..10 {
+        for j in 0..10 {
+            if let Some(_) = dots.get(&(i, j)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!("");
+    }
+
+    util::write_output(dots.len());
 }
