9576999752abb88329fe8f78c0471e3b1494f18c
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-22 17:06:44 +0200

    Implemented puzzle solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 pub mod util;
 
-fn main() {
-    let input = util::read_input();
+use std::collections::HashSet;
 
-    util::write_output(input);
+fn main() {
+    let mut vec = util::split_at::<String>("\n\n", util::read_input());
+    let mut template = vec.remove(0);
+    let rules = vec.remove(0);
+
+    let rules = util::split_lines_str(rules.to_string())
+        .into_iter()
+        .map(|l| util::split_at::<String>(" -> ", l))
+        .collect::<Vec<_>>();
+
+    const N_STEPS: i32 = 10;
+
+    for _ in 0..N_STEPS {
+        let mut new_template = "".to_string();
+        for pair in template.as_bytes().windows(2) {
+            let rule = rules.iter().find(|v| v[0].as_bytes() == pair).unwrap();
+            new_template.push(pair[0] as char);
+            new_template.push(rule[1].chars().next().unwrap());
+        }
+        new_template.push(template.chars().last().unwrap());
+        template = new_template;
+    }
+
+    let letters = template.chars().collect::<HashSet<_>>();
+    let mut occurrences: Vec<usize> = Vec::new();
+    for letter in letters {
+        occurrences.push(template.chars().filter(|c| *c == letter).count());
+    }
+    occurrences.sort();
+
+    util::write_output(occurrences.last().unwrap() - occurrences.iter().next().unwrap());
 }
