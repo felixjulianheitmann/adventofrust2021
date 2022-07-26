28fe32d1b58dd00ce1adc9dd572bdb4c9212962a
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-07-26 18:39:41 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
 
     let mut paths: HashMap<(usize, usize), Path> = HashMap::new();
 
-    let mut start = paths.insert(
+    paths.insert(
         (0, 0),
         Path {
             cost: risks[0][0],
@@ -39,57 +39,82 @@ fn main() {
         },
     );
 
-    let x_min = 0;
-    let y_min = 0;
     let x_max = risks.len();
     let y_max = risks[0].len();
 
     loop {
-        let shortespaths
+        let ((x, y), current) = paths
             .iter()
-            .min_by(|(_, lhs), (_, rhs)| lhs.cost.cmp(&rhs.cost)).unwrap();
-        for x in 0..paths.len() {
-            for y in 0..paths[0].len() {
-                if paths[x][y].evaluated {
-                    let update_neighbor = |paths: &mut Vec<Vec<Path>>,
-                                           risks: &Vec<Vec<i32>>,
-                                           x: usize,
-                                           x_inc: i32,
-                                           y: usize,
-                                           y_inc: i32| {
-                        if (x == x_min && x_inc < 0)
-                            || (y == y_min && y_inc < 0)
-                            || (y == y_max - 1 && y_inc > 0)
-                            || (x == x_max - 1 && x_inc > 0)
-                        {
-                            // Out of grid
-                            return;
-                        }
+            .min_by(|(_, lhs), (_, rhs)| lhs.cost.cmp(&rhs.cost))
+            .unwrap();
 
-                        let new_x = ((x as i32) + x_inc) as usize;
-                        let new_y = ((y as i32) + y_inc) as usize;
-                        let neighbor = &mut paths[new_x][new_y];
-                        let neighbor_risk = risks[new_x][new_y];
-                        let this = paths[x][y];
-
-                        if this.visited_nodes.contains(&(new_x, new_y)) {
-                            // This neighbor has been visited on this path already
-                            return;
-                        }
-
-                        if neighbor.evaluated && (this.cost + neighbor_risk > neighbor.cost) {
-                            // The neighbor has a better path to it's cell
-                            return;
-                        }
-
-                        // update neighbor
-                        neighbor.visited_nodes = this.visited_nodes.clone();
-                        neighbor.visited_nodes.push((x, y));
-                    };
-                }
-            }
+        if *x == x_max - 1 && *y == y_max - 1 {
+            break;
         }
+
+        update_neighbor(&mut paths, &risks, current, x_max, y_max, *x, 1, *y, 0);
+        update_neighbor(&mut paths, &risks, current, x_max, y_max, *x, -1, *y, 0);
+        update_neighbor(&mut paths, &risks, current, x_max, y_max, *x, 0, *y, 1);
+        update_neighbor(&mut paths, &risks, current, x_max, y_max, *x, 0, *y, -1);
     }
 
-    util::write_output(0);
+    let ((x, y), _) = paths
+        .iter()
+        .min_by(|(_, lhs), (_, rhs)| lhs.cost.cmp(&rhs.cost))
+        .unwrap();
+
+    // Temporary output
+    util::write_output(x * y);
+}
+
+fn update_neighbor(
+    paths: &mut HashMap<(usize, usize), Path>,
+    risks: &Vec<Vec<i32>>,
+    current: &Path,
+    x_max: usize,
+    y_max: usize,
+    x: usize,
+    x_inc: i32,
+    y: usize,
+    y_inc: i32,
+) {
+    if (x == 0 && x_inc < 0)
+        || (y == 0 && y_inc < 0)
+        || (y == y_max - 1 && y_inc > 0)
+        || (x == x_max - 1 && x_inc > 0)
+    {
+        // Out of grid
+        return;
+    }
+
+    let new_x = ((x as i32) + x_inc) as usize;
+    let new_y = ((y as i32) + y_inc) as usize;
+    let neighbor = match paths.get_mut(&(new_x, new_y)) {
+        Some(n) => n,
+        None => {
+            let mut p = Path {
+                cost: current.cost + risks[new_x][new_y],
+                visited_nodes: current.visited_nodes.clone(),
+            };
+            p.visited_nodes.push((x, y));
+            paths.insert((new_x, new_y), p);
+            return;
+        }
+    };
+
+    let neighbor_risk = risks[new_x][new_y];
+
+    if current.visited_nodes.contains(&(new_x, new_y)) {
+        // This neighbor has been visited on this path already
+        return;
+    }
+
+    if current.cost + neighbor_risk > neighbor.cost {
+        // The neighbor has a better path to it's cell
+        return;
+    }
+
+    // update neighbor
+    neighbor.visited_nodes = current.visited_nodes.clone();
+    neighbor.visited_nodes.push((x, y));
 }
